63d21ca11d046bdcfb61665818ac1df9c09075fa
Avaer Kazmer <a@modules.io>
2019-02-04 04:01:22 +0000

    Update build-ml script

--- build-ml.sh
+++ build-ml.sh
@@ -1,6 +1,6 @@
 rm -Rf CMakeCache.txt CMakeFiles webrtc/{build,src,stamp,tmp}
-# cmake -DTARGET_OS="android" -DTARGET_CPU="arm64" -DWEBRTC_REVISION="4ccc1c4bb6fe8f1681380bb9557a7fe1847562cd" # M65
-cmake -DTARGET_OS="linux" -DTARGET_CPU="arm64" -DWEBRTC_REVISION="4ccc1c4bb6fe8f1681380bb9557a7fe1847562cd" # M65
+cmake -DTARGET_OS="android" -DTARGET_CPU="arm64" -DWEBRTC_REVISION="ea143e774b4c00a74b617f272f5a8f71169cf24e" -DCMAKE_BUILD_TYPE=Debug # M65
+# cmake -DTARGET_OS="linux" -DTARGET_CPU="arm64" -DWEBRTC_REVISION="ea143e774b4c00a74b617f272f5a8f71169cf24e " # M65
 make -j4
 pushd webrtc/src/out/Release
 tar -czf lib.tar.gz obj
