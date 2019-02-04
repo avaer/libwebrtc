027687b71ef0b5bec5fdb9d232e01d0adac48796
Avaer Kazmer <a@modules.io>
2019-02-04 06:56:31 +0000

    Update build-ml script

--- build-ml.sh
+++ build-ml.sh
@@ -1,8 +1,10 @@
 rm -Rf CMakeCache.txt CMakeFiles webrtc/{build,src,stamp,tmp}
-cmake -DTARGET_OS="android" -DTARGET_CPU="arm64" -DWEBRTC_REVISION="ea143e774b4c00a74b617f272f5a8f71169cf24e" -DCMAKE_BUILD_TYPE=Debug # M65
-# cmake -DTARGET_OS="linux" -DTARGET_CPU="arm64" -DWEBRTC_REVISION="ea143e774b4c00a74b617f272f5a8f71169cf24e " # M65
+cmake -DTARGET_OS="android" -DTARGET_CPU="arm64" -DWEBRTC_REVISION="ed604f65057984d76be937130f8788d1b88094cb" -DCMAKE_BUILD_TYPE=Debug # M65
+# cmake -DTARGET_OS="linux" -DTARGET_CPU="arm64" -DWEBRTC_REVISION="ed604f65057984d76be937130f8788d1b88094cb " # M65
 make -j4
 pushd webrtc/src/out/Release
 tar -czf lib.tar.gz obj
 mv lib.tar.gz ../../../..
 popd
+
+# /home/ubuntu/android-ndk-r19/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar t libwebrtc.a >extract.sh; gawk -i inplace '{print "echo " $s " && ar x libwebrtc.a " $s " && ar d libwebrtc.a " $s " && mv " $s " " NR "-" $s}' extract.sh; chmod +x extract.sh; ./extract.sh; rm libwebrtc.a; /home/ubuntu/android-ndk-r19/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar crs libwebrtc.a *.o; /home/ubuntu/android-ndk-r19/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-objcopy libwebrtc.a; /home/ubuntu/android-ndk-r19/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-objcopy --redefine-sym isqrt32=webrtc_isqrt32 --redefine-sym silk_insertion_sort_increasing=webrtc_silk_insertion_sort_increasing --redefine-sym silk_insertion_sort_increasing_all_values_int16=webrtc_silk_insertion_sort_increasing_all_values_int16 libwebrtc.a; /home/ubuntu/android-ndk-r19/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-objcopy --redefine-syms=symbols.txt libwebrtc.a
