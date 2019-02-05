1606d60d5141b329509f2fe3a46a8282cc9f7184
Avaer Kazmer <a@modules.io>
2019-02-05 00:09:20 +0000

    Update windows build script

--- build-windows.cmd
+++ build-windows.cmd
@@ -3,9 +3,10 @@ git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools
 set PATH=%PATH%;%CD%\depot_tools
 fetch --nohooks --force webrtc
 gclient sync
+git clone https://github.com/modulesio/webrtc.git src
 pushd src
-rem M65
-git checkout 9f7e2a9
+rem M70
+git checkout ca221eabcaed7580663e3727f092e278ac56bb68
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set GYP_MSVS_VERSION=2017
 gn gen out/Release "--args=is_debug=false is_official_build=true symbol_level=0 rtc_build_examples=false rtc_build_tools=false rtc_include_tests=false"
