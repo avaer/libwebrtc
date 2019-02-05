3f5c4a4ef9dbbf8c194a555bcaf6f8279dcecdf1
Avaer Kazmer <a@modules.io>
2019-02-05 00:20:23 +0000

    Upadte linux build script

--- build-linux.sh
+++ build-linux.sh
@@ -2,8 +2,10 @@ git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools
 export PATH=$PATH:$(pwd)/depot_tools
 fetch --nohooks --force webrtc
 gclient sync
+git clone https://github.com/modulesio/webrtc.git src
 pushd src
-git checkout 9f7e2a9 # M65
+# M70
+git checkout ca221eabcaed7580663e3727f092e278ac56bb68
 gn gen out/Release "--args=is_debug=false is_official_build=true symbol_level=0 rtc_build_examples=false rtc_build_tools=false rtc_include_tests=false"
 ninja -C out/Release
 popd
