0c7e0f7ef25414c18004cd8c940d370b127ab8af
Avaer Kazmer <a@modules.io>
2019-02-07 01:36:35 +0000

    Build linux with libstc++

--- build-linux.sh
+++ build-linux.sh
@@ -6,6 +6,6 @@ git clone https://github.com/modulesio/webrtc.git src
 pushd src
 # M70
 git checkout ca221eabcaed7580663e3727f092e278ac56bb68
-gn gen out/Release "--args=is_debug=false is_official_build=true symbol_level=0 rtc_build_examples=false rtc_build_tools=false rtc_include_tests=false use_lld=false use_gold=true use_thin_lto=false is_cfi=false target_cpu=\"x64\""
+gn gen out/Release "--args=is_debug=false is_official_build=true symbol_level=0 rtc_build_examples=false rtc_build_tools=false rtc_include_tests=false use_lld=false use_gold=true use_thin_lto=false is_cfi=false use_custom_libcxx=false use_custom_libcxx_for_host=false target_cpu=\"x64\""
 ninja -C out/Release
 popd
