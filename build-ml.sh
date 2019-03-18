b4061922f255dfbcd6e7b30e6fe17c91c11543f1
Avaer Kazmer <a@modules.io>
2019-03-18 16:25:11 -0400

    Update build scripts git hashes

--- build-ml.sh
+++ build-ml.sh
@@ -4,7 +4,7 @@ export PATH="$PATH:$(pwd)/depot_tools"
 fetch --nohooks --force webrtc webrtc_android
 gclient sync
 pushd src
-git checkout 426d99e78bd2960a21dfb82d59a2aff68c3816e1
+git checkout 77de4050abfbbfaaa036cc7526c078bb2c94308c
 gn gen out/Release_x64 '--args=is_debug=false symbol_level=0 rtc_build_examples=false rtc_build_tools=false rtc_include_tests=false target_os="android" target_cpu="arm64"'
 ninja -C out/Release_x64
 popd
