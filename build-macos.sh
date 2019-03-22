616688b0bc7ee3652632f621b2000c90168b437d
Avaer Kazmer <a@modules.io>
2019-03-22 10:21:57 -0400

    Update macos build script ninja target path

--- build-macos.sh
+++ build-macos.sh
@@ -6,5 +6,5 @@ pushd src
 # M71
 git checkout ce65c12e9b40596bd977c14cfb2fc6c82a10acaf
 gn gen out/Release_x64 "--args=is_debug=false symbol_level=0 rtc_build_examples=false rtc_build_tools=false rtc_include_tests=false"
-ninja -C out/Release
+ninja -C out/Release_x64
 popd
